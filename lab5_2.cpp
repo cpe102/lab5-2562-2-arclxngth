4030164a6ab06eba55c75997573ac03978342d26
arclxngth <59277471+arclxngth@users.noreply.github.com>
2020-01-07 15:41:53 +0700

    version 1

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,7 +1,26 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
+string mixText(string x, string y){
 
+	int L=x.size();
+	int N=y.size();
+
+	if(L==N){
+		int i=0;
+		string s;
+		while(i<L){
+			
+			s = s+x[i]+y[i];
+			i++;
+		}
+		return s;
+	}
+	else{
+		return "E";
+	}
+}
 
 
 int main(){	
